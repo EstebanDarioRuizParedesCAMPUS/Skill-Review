62a99cbdb8ad777d52e1b63996682d68a85b65c2
EstebanDarioRuizParedesCAMPUS <123566003+EstebanDarioRuizParedesCAMPUS@users.noreply.github.com>
2023-02-07 13:17:29 -0500

    Update Ejercicio10.py

--- Python/Skills Review Introducción/Ejercicio10.py
+++ Python/Skills Review Introducción/Ejercicio10.py
@@ -7,5 +7,9 @@ siguientes:
 
 Meses = ["Null","Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
 Estrato = ["null",1,2,3,4,5,6]
+vkw = ["null",315.6,394.5,667.9,785.8,942.9,942.9]
+numes = int(input(f"Ingrese en que mes estás (números): "))
+numestratro = int(input(f"Ingresa tu estrato: "))
+kwusado = int(input(f"¿Cúantos kw has usado en el mes? "))
 
-[]
+print(f"En {Meses[numes]} debes pagar un total de COP${vkw[numestratro]*kwusado} al estar en estrato {Estrato[numestratro]}")
